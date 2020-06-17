dc65ff19ee8daf3fe5a1d15b4de6b240f4463b26
Qixiang Cheng <qixcheng@microsoft.com>
2020-06-17 22:56:45 +0800

    Fix

--- browse.ps1
+++ browse.ps1
@@ -5,38 +5,39 @@ function Test-Administrator {
 
 function Invoke-Administrator {
     $argumentList = @('-File', $Script:MyInvocation.MyCommand.Path) + $Script:args
-    Start-Process -FilePath "PowerShell.exe" `
+    Start-Process -FilePath 'PowerShell.exe' `
                   -ArgumentList $argumentList `
                   -Verb RunAs
 }
 
 function Register-Browse {
-    New-Item -Path "Registry::HKEY_CLASSES_ROOT\browse" -Force
-    New-ItemProperty -Path "Registry::HKEY_CLASSES_ROOT\browse" `
-        -Name "URL Protocol" `
-        -Value ""
-    New-Item -Path "Registry::HKEY_CLASSES_ROOT\browse\shell\open\command" `
-        -Value "PowerShell.exe -File ""$($Script:MyInvocation.MyCommand.Path)"" %1" `
-        -Force
+    New-Item -Path 'Registry::HKEY_CLASSES_ROOT\browse' -Force
+    New-ItemProperty -Path 'Registry::HKEY_CLASSES_ROOT\browse' `
+                     -Name 'URL Protocol' `
+                     -Value ''
+    New-Item -Path 'Registry::HKEY_CLASSES_ROOT\browse\shell\open\command' `
+             -Value "PowerShell.exe -File ""$($Script:MyInvocation.MyCommand.Path)"" %1" `
+             -Force
 
-    Write-Host -Object "Register successfully, trying ""browse:file:///C:/Program%20Files"""
-    Start-Process -FilePath "browse:file:///C:/Program%20Files"
+    Write-Host -Object 'Register successfully, trying "browse:file:///C:/Program%20Files"'
+    Start-Process -FilePath 'browse:file:///C:/Program%20Files'
 }
 
 function Unregister-Browse {
-    Remove-Item -Path "Registry::HKEY_CLASSES_ROOT\browse" -Recurse
-    Write-Host -Object "Deregister successfully"
+    Remove-Item -Path 'Registry::HKEY_CLASSES_ROOT\browse' -Recurse
+    Write-Host -Object 'Unregister successfully'
 }
 
 function Invoke-Browse {
     Add-Type -AssemblyName System.Web
     $path = $Script:args[0] -CReplace '^browse:', ''
     $path = [System.Web.HttpUtility]::UrlDecode($path)
-    Write-Host "Opening", $path
-    Start-Process -FilePath $path
+    Write-Host 'Opening', $path
+    Start-Process -FilePath 'Explorer.exe' `
+                  -ArgumentList $path
 }
 
-if ($null -eq $args[0] -or "register" -eq $args[0]) {
+if ($null -eq $args[0] -or 'register' -eq $args[0]) {
     if (Test-Administrator) {
         Register-Browse
     }
@@ -44,7 +45,7 @@ if ($null -eq $args[0] -or "register" -eq $args[0]) {
         Invoke-Administrator
     }
 }
-elseif ("unregister" -eq $args[0]) {
+elseif ('unregister' -eq $args[0]) {
     if (Test-Administrator) {
         Unregister-Browse
     }
